c715b951647a640636ce200514f87f9f30535fdf
TEMichaST <118178476+TEMichaST@users.noreply.github.com>
2022-11-14 12:57:19 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,16 +26,25 @@ let by = 0
 let x1 = 0
 let x = 0
 let bx1 = 0
-let by1 = 0
+let by1 = 1
 let sx = 0
 x = 0
 x1 = 1
 let bx = 2
 let sy = 1000
 let S = sx + sy
+led.plot(bx, by)
+led.plot(x, 4)
+led.plot(x1, 4)
 loops.everyInterval(S, function () {
     led.plot(bx, by)
-    S += -10
-    bx += 1
-    by += 1
+    led.unplot(bx, by)
+    S += -100
+    bx += bx1
+    by += by1
+    if ((by1 == x - 1 || by1 == x1 - 1) && (bx == x || bx == x1)) {
+        bx1 = bx1 * -1
+        by1 = by1 * -1
+    }
+    led.plot(bx, by)
 })
